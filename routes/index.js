b614373c06ca63fcd2f55841e77753ddb32953d9
docaotri123 <docaotri211997@gmail.com>
2018-06-28 21:49:43 +0700

    make it better

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,36 @@
 var express = require('express');
 var router = express.Router();
-var cake_Controller=require('../controllers/cakeController');
+var cake_Controller = require('../controllers/cakeController');
 var Product = require('../models/cake');
 var Cart = require('../models/cart');
-const Bill=require('../models/bill');
-const BillInfo=require('../models/billInfo');
+const Bill = require('../models/bill');
+const BillInfo = require('../models/billInfo');
 // GET home page.
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.redirect('/catalog');
 });
 
 
 
-router.get('/checkout',(req,res)=>{
-  res.render('partials/checkout',{title:'Checkout'});
+router.get('/checkout', (req, res) => {
+  res.render('partials/checkout', { title: 'Checkout' });
 });
 
-router.get('/myaccount',(req,res)=>{
-  res.render('partials/myaccount',{title:'My Account'});
+router.get('/myaccount', (req, res) => {
+  res.render('partials/myaccount', { title: 'My Account' });
 });
 
-router.get('/about',cake_Controller.about);
+router.get('/about', cake_Controller.about);
 
 
-router.get('/contact',cake_Controller.contact);
+router.get('/contact', cake_Controller.contact);
 
-router.post('/add-to-card/:id', function(req,res,next){
+router.post('/add-to-card/:id', function (req, res, next) {
   var productId = req.params.id;
   console.log(req.params.id);
-  var cart = new Cart(req.session.cart? req.session.cart:{});
-  Product.findById(productId, function(err, product){
-    if(err){
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+  Product.findById(productId, function (err, product) {
+    if (err) {
       return res.redirect('/');
     }
     cart.add(product, productId);
@@ -38,33 +38,40 @@ router.post('/add-to-card/:id', function(req,res,next){
     console.log(req.session.cart);
     res.redirect('/');
   });
-  
+
 });
 
-router.get('/shopping-cart', function(req,res,next){
-  if(!req.session.cart){
-    return res.render('partials/shopping-cart', {products: null});
+router.get('/shopping-cart', function (req, res, next) {
+  if (!req.session.cart) {
+    return res.render('partials/shopping-cart', { products: null });
   }
   var cart = new Cart(req.session.cart);
-  res.render('partials/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+  res.render('partials/shopping-cart', { products: cart.generateArray(), totalPrice: cart.totalPrice });
 });
 
-router.post('/payment',(req,res)=>{
-  if(res.locals.login)
-  {    
+router.post('/payment', (req, res) => {
+  if (res.locals.login) {
     //create bill
-    let bill=Bill({dateCheckIn:Date.now(),dateCheckOut:null,disCount:0,status:'Chưa thanh toán',address:req.param('address'),customer:res.locals.user});
+    let bill = Bill({ dateCheckIn: Date.now(), dateCheckOut: null, disCount: 0, status: 'Chưa thanh toán', address: req.param('address'), customer: res.locals.user });
+    // bill.save(err => {
+    //   if (err)
+    //     return err;
+    // });
+    // //create billInfo
+    // const items = req.session.cart.items;
     
-    //create billInfo
-    for(let item in req.session.cart.items)
-    {
-      console.log(item);
-    }
-    req.session.cart=null;
+    // let i=0;
+    // for (let item in items) {
+    //   let billinfos = new BillInfo({ cake: items[item].item, bill: bill, priceNow: items[item].price, count: items[item].qty });
+    //   billinfos.save();
 
+    // }
+    // console.log(billinfos);
+    
+    req.session.cart=null;
     res.redirect('/shopping-cart');
-  }else{
+  } else {
     res.redirect('/shopping-cart');
   }
 });
-module.exports=router;
+module.exports = router;
